0f8975aa24908a31b999cd49ea41b87a4aaa5113
jeffreyadamanderson-hash <jeffreyadamanderson@gmail.com>
2025-08-26 16:49:14 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AlbumDemo from './AlbumDemo.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AlbumDemo />
+  </React.StrictMode>,
+)
